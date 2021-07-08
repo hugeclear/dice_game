ccb7810d3eb5f65ec8db354721e1ae01c4e6a8c5
nakata_taisei” <nakata_taisei@plise.co.jp>
2021-07-08 17:28:25 +0900

    Won/lost message modified

---
+++ dice_game.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+//サイコロ
+int main(){
+    int a1,a2,judge,a[10];
+     printf("What is your name?\n>");
+    scanf("%s",&a);
+    printf("Hello %s\n",a);
+    printf("Rolling the dice...\n");
+
+    srand(time(NULL));
+
+    a1=rand()%6+1;
+    a2=rand()%6+1;
+
+    printf("Die 1: %d\n",a1);
+    printf("Die 2: %d\n",a2);
+
+
+    printf("Total value: %d\n",judge);
+
+    if(judge>7){
+        printf("&s won\n",a);
+    }
+        else {
+        printf("%s lost\n",a);
+        
+    }
+    
+
+}
